427efd72b691bda62ad90e51da2a6fb8ece2f5a5
Sholto <madein.mania@gmail.com>
2021-09-24 09:11:19 +0100

    updated cv link

--- src/routes/cv/index.svelte
+++ src/routes/cv/index.svelte
@@ -9,7 +9,7 @@
     <h1 class="page-intro">CV</h1>
     <div class="cv">
       <h3>
-        You can download the latest copy of my CV <a rel="external" target="_blank" href="/cv0621.pdf">here!</a>
+        You can download the latest copy of my CV <a rel="external" target="_blank" href="/cv.pdf">here!</a>
       </h3>
     </div>
   </main>
