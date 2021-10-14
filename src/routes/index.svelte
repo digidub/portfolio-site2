d748c0702292760539680369ddb0e320dcdad18b
Sholto <madein.mania@gmail.com>
2021-10-14 13:56:28 +0100

    homepage tweak

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,7 +6,9 @@
 
 <PageSlide>
   <main>
-    <h1 class="page-intro">I taught myself to build websites, such as this one.</h1>
+    <h1 class="page-intro">
+      I taught myself to build websites, such as this one.
+    </h1>
 
     <PageSlide>
       <div class="skills">
@@ -16,7 +18,9 @@
         </section>
         <section>
           <h2>...and I know how things work under the hood.</h2>
-          <p>Test-Driven Development (TDD), NPM, Webpack, Git, Firebase, Linux</p>
+          <p>
+            Test-Driven Development (TDD), NPM, Webpack, Git, Firebase, Linux
+          </p>
         </section>
         <section>
           <h2>But I'm not stopping there!</h2>
@@ -25,8 +29,12 @@
         <section>
           <h2>Though I am ready to offer my skills now:</h2>
           <p>
-            Why not check out my <a target="_self" href="/projects">portfolio</a> and
-            <a target="_self" href="/cv">CV</a>? Or <a target="_self" href="/contact">contact</a> me about a job?
+            Why not check out my <a target="_self" href="/projects"
+              >portfolio</a
+            >
+            and
+            <a target="_self" href="/cv">CV</a>? Or
+            <a target="_self" href="/contact">contact me</a> about a job?
           </p>
         </section>
       </div>
